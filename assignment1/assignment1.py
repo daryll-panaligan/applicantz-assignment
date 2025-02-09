c2d303f0ae1f8e1abb5e063d5d2cc6e23e3a77af
Daryll Panaligan <50567021+daryll-panaligan@users.noreply.github.com>
2025-02-09 16:48:08 +0800

    Convert to python code

---
+++ assignment1/assignment1.py
@@ -0,0 +1,13 @@
+def reverse_words(input_str):
+    # TODO: Implement this function
+    return "???"
+
+
+def main():
+    test_str = "String; 2be reversed..."
+    assert reverse_words(test_str) == "gnirtS; eb2 desrever..."
+    return 0
+
+
+if __name__ == "__main__":
+    main()
