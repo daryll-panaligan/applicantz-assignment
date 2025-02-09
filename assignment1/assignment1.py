15141758daf12dfab6d5f871c7320c7b5075f338
Daryll Panaligan <50567021+daryll-panaligan@users.noreply.github.com>
2025-02-09 16:55:31 +0800

    Implement reverse words using regex

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,12 +1,24 @@
+import re
+
+
 def reverse_words(input_str):
-    # TODO: Implement this function
-    return "???"
+    # Match letters and digits only
+    regex_str = "[A-z0-9]+"
+    subs_to_be_reversed = re.findall(regex_str, input_str)
+
+    print(subs_to_be_reversed)
+
+    # Reverse each matching substring in input_str iteratively
+    reversed_str = input_str
+    for substr in subs_to_be_reversed:
+        reversed_str = re.sub(substr, substr[::-1], reversed_str)
+
+    return reversed_str
 
 
 def main():
     test_str = "String; 2be reversed..."
     assert reverse_words(test_str) == "gnirtS; eb2 desrever..."
-    return 0
 
 
 if __name__ == "__main__":
