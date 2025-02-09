c7ca9b33d956c6685614277bf5240d3a7c4a6afe
Daryll Panaligan <50567021+daryll-panaligan@users.noreply.github.com>
2025-02-09 21:32:50 +0800

    Create unit tests
    - add test files

--- assignment2/test_assignment2.py
+++ assignment2/test_assignment2.py
@@ -1,5 +1,35 @@
+import filecmp
+import os
+import shutil
+
+import pytest
+
 import assignment2
 
 
-def test_update_build_number():
-    assert False
+def copy_to_temp_file(tmp_path, file_path):
+    tmp_file_path = os.path.join(tmp_path, file_path)
+    shutil.copyfile(file_path, tmp_file_path)
+    return tmp_file_path
+
+@pytest.fixture
+def input_file(tmp_path, request):
+    input_file_src = request.param[0]
+    tmp_file_path = copy_to_temp_file(tmp_path, input_file_src)
+
+    expected_file_src = request.param[2]
+    tmp_file_result_path = copy_to_temp_file(tmp_path, expected_file_src)
+
+    return tmp_file_path, request.param[1], tmp_file_result_path
+
+@pytest.mark.parametrize('input_file',
+                         [['VERSION', '', 'VERSION_expected'],
+                          ['SConstruct', '', 'SConstruct_expected']],
+                         indirect=True)
+def test_update_build_number(input_file):
+    path = input_file[0]
+    pattern = input_file[1]
+    result = input_file[2]
+    buildNum = 123
+    assignment2.updateBuildNumber(path, pattern, buildNum)
+    assert filecmp.cmp(path, result)
