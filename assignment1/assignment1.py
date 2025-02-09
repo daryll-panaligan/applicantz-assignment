1945400ef0117aab1b6d2e3084832bcb4997065d
Daryll Panaligan <50567021+daryll-panaligan@users.noreply.github.com>
2025-02-09 17:21:56 +0800

    Initialize regex into `Pattern` object instead

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -3,15 +3,15 @@ import re
 
 def reverse_words(input_str):
     # Match letters and digits only
-    regex_str = "[A-z0-9]+"
-    subs_to_be_reversed = re.findall(regex_str, input_str)
+    reg = re.compile(r"[A-z0-9]+")
+    subs_to_be_reversed = reg.findall(input_str)
 
     print(subs_to_be_reversed)
 
     # Reverse each matching substring in input_str iteratively
     reversed_str = input_str
     for substr in subs_to_be_reversed:
-        reversed_str = re.sub(substr, substr[::-1], reversed_str)
+        reversed_str = reversed_str.replace(substr, substr[::-1])
 
     return reversed_str
 
